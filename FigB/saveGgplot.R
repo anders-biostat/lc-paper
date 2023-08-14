1daaec9019a9ca723848a800658cda568b03f084
Svetlana <tilakloivenn@gmail.com>
2023-08-14 17:51:55 +0200

    Some figures updated

--- FigB/saveGgplot.R
+++ FigB/saveGgplot.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 
-png("ggplot.png", width = 600, height = 500)
+png("ggplot.png", width = 900, height = 750)
 ggplot(iris) + geom_point(aes(x = Sepal.Length, y = Petal.Length,
-                              size = Sepal.Width * 2, colour = Petal.Width, shape = Species))
+                              size = Sepal.Width * 2, colour = Petal.Width, shape = Species)) +
+  theme(text = element_text(size = 27))
 dev.off()
